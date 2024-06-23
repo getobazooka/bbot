94c7eca500fb136c1f1508fc6c73efbb308f8346
getobazooka <getobazooka@gmail.com>
2024-06-23 17:41:35 +0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,36 @@
+  <?php
+// Load composer
+use Longman\TelegramBot\Request;
+
+require __DIR__ . '/vendor/autoload.php';
+
+$bot_api_key  = '7305808870:AAGs2QzhNqsfrWyjdXfP4Wep5EZPd73HulY';
+$bot_username = 'testakarbot';
+$hook_url     = 'https://stackme.online/test/bbot.php';
+$hook_url     = 'https://my-appakar.adaptable.app/';
+
+try {
+    // Create Telegram API object
+    $telegram = new Longman\TelegramBot\Telegram($bot_api_key, $bot_username);
+
+//    $result = Request::sendMessage([
+//        'chat_id' => $chat_id,
+//        'text'    => 'Your utf8 text 😜 ...',
+//    ]);
+    // Set webhook
+   // $result = $telegram->setWebhook($hook_url);
+//    if ($result->isOk()) {
+//        echo $result->getDescription();
+//    }
+    $chat_id = '729499405';
+    $result = Request::sendMessage([
+        'chat_id' => $chat_id,
+        'text'    => 'slaw',
+    ]);
+} catch (Longman\TelegramBot\Exception\TelegramException $e) {
+    // log telegram errors
+    // echo $e->getMessage();
+}
+
+echo 'hello worldx';
+  ?>
